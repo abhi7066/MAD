022689eb1462b082deb6896757546827fde15ea5
Abhishek Dethe <abhishekdethe7066@gmail.com>
2023-11-02 02:07:57 +0530

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MCAII"
+include(":app")
+include(":app:question1")
+include(":app:question2")
+include(":app:question3a")
+include(":app:question3b")
+include(":app:question4")
+include(":app:question5")
+include(":app:question6")
+include(":app:question8")
